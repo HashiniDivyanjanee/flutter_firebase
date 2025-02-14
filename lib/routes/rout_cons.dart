c9f48a0f7b79ccdbc628f29ebbc422c79cf5afcd
Hashini Divyanjanee <143160465+HashiniDivyanjanee@users.noreply.github.com>
2025-02-14 15:19:38 +0530

    Remove SignInScreen and SignUpScreen; update routing constants to reflect changes

--- lib/routes/rout_cons.dart
+++ lib/routes/rout_cons.dart
@@ -3,6 +3,4 @@ class AppRoutesConstants {
   static const String home = '/home';
   static const String login = '/login';
   static const String sign_up = '/sign_up';
-  static const String test1 = '/test1';
-  static const String test2 = '/test2';
 }
