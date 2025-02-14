cb0cef59ae771fd413d4e2a754724401572d04e7
Hashini Divyanjanee <143160465+HashiniDivyanjanee@users.noreply.github.com>
2025-02-14 09:58:54 +0530

    Add sign-up and Google sign-in functionality to AuthBloc

--- lib/bloc/auth/auth_bloc.dart
+++ lib/bloc/auth/auth_bloc.dart
@@ -10,6 +10,8 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   AuthBloc() : super(AuthInitial()) {
     on<SignInWithEmail>(_onSignInWithEmail);
+    on<SignUpWithEmail>(_onSignUpWithEmail);
+    on<SignInWithGoogle>(_onsignInWithGoogle);
   }
 
   Future<void> _onSignInWithEmail(
@@ -24,4 +26,45 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       emit(AuthFailure(e.toString()));
     }
   }
+
+  Future<void> _onSignUpWithEmail(
+    SignUpWithEmail event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    try {
+      final UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+              email: event.email, password: event.password);
+      emit(AuthSuccess(userCredential.user!));
+    } catch (e) {
+      emit(AuthFailure(e.toString()));
+    }
+  }
+
+  Future<void> _onsignInWithGoogle(
+      SignInWithGoogle event, Emitter<AuthState> emit) async {
+        emit(AuthLoading());
+        try {
+          final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+          if (googleUser != null) {
+           final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+          final AuthCredential credential = GoogleAuthProvider.credential(
+            accessToken: googleAuth.accessToken,
+            idToken: googleAuth.idToken,
+          );
+          final UserCredential userCredential = await _auth.signInWithCredential(credential);
+          emit(AuthSuccess(userCredential.user!));
+          }
+          
+        } catch (e) {
+          emit(AuthFailure(e.toString()));
+        }
+      }
+
+      Future<void>_onSignOut(SignOut event, Emitter<AuthState> emit)async{
+        await _auth.signOut();
+        await _googleSignIn.signOut();
+        emit(AuthInitial());
+      }
 }
