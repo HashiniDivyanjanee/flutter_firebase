04fe7c3e458e7e49229d801e201ba2a902b9780d
Hashini Divyanjanee <143160465+HashiniDivyanjanee@users.noreply.github.com>
2025-02-18 13:13:01 +0530

    Add CustomerRepository for CRUD operations on customer data

---
+++ lib/repository/customer_repo.dart
@@ -0,0 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_firebase/models/customer.dart';
+
+class CustomerRepository {
+  final CollectionReference _custCollection =
+      FirebaseFirestore.instance.collection('customer');
+
+  Future<void> saveCustomer(
+    String name,
+    String nic,
+    String phone,
+    String landline,
+    String address,
+  ) async {
+    await _custCollection.add(Customer(
+            name: name,
+            nic: nic,
+            phone: phone,
+            landline: landline,
+            address: address)
+        .toJson());
+  }
+
+  Future<List<Customer>> loadCustomer() async {
+    final snapshot = await _custCollection.orderBy('cid').get();
+    return snapshot.docs
+        .map((doc) =>
+            Customer.fromJson(doc.data() as Map<String, dynamic>, doc.id))
+        .toList();
+  }
+
+  Future<void> updateCustomer(
+    String cid,
+    String name,
+    String nic,
+    String phone,
+    String landline,
+    String address,
+  ) async {
+    await _custCollection.doc(cid).update({
+      name: name,
+      nic: nic,
+      phone: phone,
+      landline: landline,
+      address: address
+    });
+  }
+
+  Future<void> deleteCustomer(String cid) async {
+    await _custCollection.doc(cid).delete();
+  }
+}
