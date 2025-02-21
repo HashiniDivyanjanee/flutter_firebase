e8633e66ae6f502194775c599757b991b599439b
Hashini Divyanjanee <143160465+HashiniDivyanjanee@users.noreply.github.com>
2025-02-21 17:25:36 +0530

    Add edit and delete functionality to customer list items; enhance ListItemWidget to support callbacks for editing and deleting customers.

--- lib/presentation/screen/customer/customer_view.dart
+++ lib/presentation/screen/customer/customer_view.dart
@@ -5,13 +5,19 @@ import 'package:flutter_firebase/widget/app_bar_widget.dart';
 import 'package:flutter_firebase/widget/list_item_widget.dart';
 
 class CustomerView extends StatefulWidget {
-  const CustomerView({super.key});
+  CustomerView({super.key});
 
   @override
   State<CustomerView> createState() => _CustomerViewState();
 }
 
 class _CustomerViewState extends State<CustomerView> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _nicController = TextEditingController();
+  final TextEditingController _landlineController = TextEditingController();
+
   void initState() {
     super.initState();
     BlocProvider.of<CustomerBloc>(context).add(LoadCustomerEvent());
@@ -30,12 +36,78 @@ class _CustomerViewState extends State<CustomerView> {
           return ListView.builder(
               itemCount: state.customer.length,
               itemBuilder: (context, index) {
-                return ListItemWidget(data: state.customer[index]);
+                return ListItemWidget(
+                  data: state.customer[index],
+                  callback_edit: () {
+                    _nameController.text = state.customer[index].name;
+                    _phoneController.text = state.customer[index].phone;
+                    _addressController.text = state.customer[index].address;
+                    _nicController.text = state.customer[index].nic;
+                    _landlineController.text = state.customer[index].landline;
+                    showDialog(
+                      context: context,
+                      builder: (dialogContext) => AlertDialog(
+                        title: Text('Edit Item'),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TextField(
+                              controller: _nameController,
+                              decoration:
+                                  const InputDecoration(labelText: 'Name'),
+                            ),
+                            TextField(
+                              controller: _phoneController,
+                              decoration: const InputDecoration(
+                                  labelText: 'Phone Number'),
+                            ),
+                            TextField(
+                              controller: _landlineController,
+                              decoration: const InputDecoration(
+                                  labelText: 'Landline Number'),
+                            ),
+                            TextField(
+                              controller: _addressController,
+                              decoration:
+                                  const InputDecoration(labelText: 'Address'),
+                            ),
+                            TextField(
+                              controller: _nicController,
+                              decoration: const InputDecoration(
+                                  labelText: 'NIC Number'),
+                            ),
+                          ],
+                        ),
+                        actions: [
+                          TextButton(
+                              onPressed: () {
+                                BlocProvider.of<CustomerBloc>(dialogContext)
+                                    .add(UpdateCustomerEvent(
+                                  state.customer[index].cid!,
+                                  _nameController.text,
+                                  _nicController.text,
+                                  _phoneController.text,
+                                  _landlineController.text,
+                                  _addressController.text,
+                                ));
+                                Navigator.pop(context);
+                              },
+                              child: Text('Update'))
+                        ],
+                      ),
+                    );
+                  },
+                  callback_delete: () {
+                    context
+                        .read<CustomerBloc>()
+                        .add(DeleteCustomerEvent(state.customer[index].cid!));
+                  },
+                );
               });
         } else if (state is CustomerError) {
           return Center(child: Text('Error: ${state.message}'));
         }
-         return Center(child: Text("No data available"));
+        return Center(child: Text("No data available"));
       }),
     );
   }
--- lib/widget/list_item_widget.dart
+++ lib/widget/list_item_widget.dart
@@ -4,53 +4,59 @@ import 'package:flutter_firebase/models/customer.dart';
 
 class ListItemWidget extends StatelessWidget {
   final Customer data;
-
-  const ListItemWidget({super.key, required this.data});
+  final VoidCallback callback_edit;
+   final VoidCallback callback_delete;
+  const ListItemWidget({super.key, required this.data, required this.callback_edit, required this.callback_delete});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-    
       color: AppThemes.cardBackgroundColor,
       margin: const EdgeInsets.all(5),
       elevation: 8,
       shadowColor: AppThemes.PrimaryColor,
-     
       child: ListTile(
         dense: true,
-        contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
         title: Text(
           data.name,
-          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
         ),
         subtitle: Column(
           children: [
             Row(
               children: [
                 Text(
-                  data.nic,
+                  data.phone,
                   style: const TextStyle(
                       color: AppThemes.PrimaryColor,
                       fontWeight: FontWeight.bold,
                       fontSize: 15),
                 ),
                 const Spacer(),
-                Text(data.phone,
+                Text(
+                  data.address,
                   style: const TextStyle(
-                      color: AppThemes.PrimaryColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15),
+                      fontWeight: FontWeight.bold, fontSize: 15),
                 ),
               ],
             ),
             Row(
               children: [
                 const Spacer(),
-                Text(
-                  data.address,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
+                IconButton(
+                  icon: Icon(
+                    Icons.edit,
                   ),
+                  onPressed: callback_edit,
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.delete,
+                    color: const Color.fromARGB(255, 207, 22, 9),
+                  ),
+                  onPressed: callback_delete,
                 )
               ],
             )
